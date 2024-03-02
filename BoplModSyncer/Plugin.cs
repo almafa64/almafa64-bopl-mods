7127d0510fbeaebea81eb2d5b668554c7044cc41
almafa64 <bhatalyak@gmail.com>
2024-03-02 20:52:50 +0100

    Checksum calculation

--- BoplModSyncer/Plugin.cs
+++ BoplModSyncer/Plugin.cs
@@ -1,13 +1,39 @@
 ﻿using BepInEx;
+using BepInEx.Bootstrap;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Collections.Generic;
 
-namespace BoplBattleTemplate
+namespace BoplModSyncer
 {
-    [BepInPlugin("com.almafa64.BoplModSyncer", "BoplModSyncer", "1.0.0")]
-    public class Plugin : BaseUnityPlugin
-    {
-        private void Awake()
-        {
-            
-        }
-    }
+	[BepInPlugin("com.almafa64.BoplModSyncer", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+	public class Plugin : BaseUnityPlugin
+	{
+		internal static Harmony harmony;
+		internal static ManualLogSource logger;
+		internal static string _checksum;
+		public static string CHECKSUM { get => _checksum ?? throw new("CHECKSUM hasn't been calculated"); }
+
+		private void Awake()
+		{
+			harmony = new(Info.Metadata.GUID);
+			logger = Logger;
+		}
+
+		private void Start()
+		{
+			_checksum = Utils.CombineHashes(GetModHashes());
+		}
+
+		private List<string> GetModHashes()
+		{
+			List<string> hashes = [];
+			foreach (BepInEx.PluginInfo plugin in Chainloader.PluginInfos.Values)
+			{
+				string hash = Utils.Checksum(plugin.Location);
+				hashes.Add(hash);
+			}
+			return hashes;
+		}
+	}
 }
---
+++ BoplModSyncer/Utils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoplModSyncer
+{
+	internal class Utils
+	{
+		public static string Checksum(string path)
+		{
+			using SHA256 algorithm = SHA256.Create();
+			using FileStream stream = File.OpenRead(path);
+			byte[] bytes = algorithm.ComputeHash(stream);
+			string tmp = BitConverter.ToString(bytes).Replace("-", "") + stream.Length;
+			return tmp;
+		}
+
+		public static string CombineHashes(List<string> hashes)
+		{
+			StringBuilder sb = new();
+			foreach (string hash in hashes)
+			{
+				sb.Append(hash);
+			}
+			using(SHA256 hash = SHA256.Create())
+			{
+				byte[] bytes = hash.ComputeHash(Encoding.UTF8.GetBytes(sb.ToString()));
+				return BitConverter.ToString(bytes).Replace("-", "");
+			}
+		}
+	}
+}
