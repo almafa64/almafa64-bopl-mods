39fed289d68a7041c7aaf29a146b4508ca5512d3
almafa64 <bhatalyak@gmail.com>
2024-03-04 00:21:36 +0100

    Updated template

--- BoplBattleTemplate/Plugin.cs
+++ BoplBattleTemplate/Plugin.cs
@@ -1,13 +1,29 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
 
 namespace BoplBattleTemplate
 {
-	[BepInPlugin("com.CREATOR_NAME.PLUGIN_NAME", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+	[BepInPlugin("com.almafa64.PLUGIN_NAME", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
 	public class Plugin : BaseUnityPlugin
 	{
+		internal static Harmony harmony;
+		internal static ManualLogSource logger;
+		internal static ConfigFile config;
+
 		private void Awake()
 		{
+			harmony = new(Info.Metadata.GUID);
+			logger = Logger;
+			config = Config;
+
 			Logger.LogMessage($"guid: {Info.Metadata.GUID}, name: {Info.Metadata.Name}, version: {Info.Metadata.Version}");
 		}
+	}
+
+	class Patches
+	{
+		
 	}
 }
