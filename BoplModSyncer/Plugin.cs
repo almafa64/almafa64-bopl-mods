e8d67aa5b41923d4aaa5d0b19f872ebf30c79765
almafa64 <bhatalyak@gmail.com>
2024-03-04 00:23:17 +0100

    [BoplModSyncer] Added checksum text field to main menu

--- BoplModSyncer/Plugin.cs
+++ BoplModSyncer/Plugin.cs
@@ -3,26 +3,35 @@ using BepInEx.Bootstrap;
 using BepInEx.Logging;
 using HarmonyLib;
 using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace BoplModSyncer
 {
 	[BepInPlugin("com.almafa64.BoplModSyncer", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
 	public class Plugin : BaseUnityPlugin
 	{
+		public static string CHECKSUM { get => _checksum ?? throw new("CHECKSUM hasn't been calculated"); }
+		
 		internal static Harmony harmony;
 		internal static ManualLogSource logger;
 		internal static string _checksum;
-		public static string CHECKSUM { get => _checksum ?? throw new("CHECKSUM hasn't been calculated"); }
+
+		private TextMeshProUGUI checksumText;
 
 		private void Awake()
 		{
 			harmony = new(Info.Metadata.GUID);
 			logger = Logger;
+
+			SceneManager.sceneLoaded += OnSceneLoaded;
 		}
 
 		private void Start()
 		{
 			_checksum = Utils.CombineHashes(GetModHashes());
+			SetChecksumText();
 		}
 
 		private List<string> GetModHashes()
@@ -34,6 +43,27 @@ namespace BoplModSyncer
 				hashes.Add(hash);
 			}
 			return hashes;
+		}
+
+		private void SetChecksumText()
+		{
+			checksumText.text = CHECKSUM;
+			checksumText.fontSize -= 5;
+			Vector3 pos = Camera.main.WorldToScreenPoint(checksumText.transform.position);
+			pos.y = 10;
+			checksumText.transform.position = Camera.main.ScreenToWorldPoint(pos);
+		}
+
+		private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+		{
+			if (scene.name != "MainMenu") return;
+			Transform canvas = GameObject.Find("Canvas (1)").transform;
+			GameObject exitText = GameObject.Find("ExitText");
+			GameObject hashObj = Instantiate(exitText, canvas);
+			Destroy(hashObj.GetComponent<LocalizedText>());
+			checksumText = hashObj.GetComponent<TextMeshProUGUI>();
+			checksumText.transform.position = exitText.transform.position;
+			if (_checksum != null) SetChecksumText();
 		}
 	}
 }
