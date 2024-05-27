ceeda7b60af8886168f51e90cc021a8ea67e1efa
almafa64 <bhatalyak@gmail.com>
2024-05-27 20:58:40 +0200

    [Wormhole] extracted methods

--- Wormhole/Patches.cs
+++ Wormhole/Patches.cs
@@ -58,74 +58,86 @@ namespace Wormhole
 			whiteHoles.Clear();
 		}
 
-		internal static bool BlackHoleCollide_Prefix(BlackHole __instance, CollisionInformation collision)
+		private static void TeleportPlayer(GameObject collidedObject, BlackHole pair)
 		{
-			Fix selfMass = GetMass(__instance);
-			if (selfMass < Fix.Zero) return true;
-
-			GameObject collidedObject = collision.colliderPP.fixTrans.gameObject;
-
-			if (collision.layer == (int)affectorLayerField.GetValue(__instance))
-			{
-				BlackHole component = collidedObject.GetComponent<BlackHole>();
-				Fix compMass = GetMass(component);
-				if (selfMass > compMass || (compMass == selfMass && __instance.spriteRen.sortingOrder > component.spriteRen.sortingOrder))
-				{
-					// eated another blackhole
-					RemoveConnection(component, false);
-					whiteHoles.Remove(component);
-					holePairs.Remove(component);
-				}
-				return true;
-			}
-
-			// only run original code if this hole isnt paired
-			if (!holePairs.TryGetValue(__instance, out BlackHole holePair) || holePair == null) return true;
-
-			//Fix holeRadius = __instance.dCircle.radius * (__instance.influenceRadiusMultiplier / (Fix)2);
-			Fix holeRadius = __instance.dCircle.radius;
-
-			if (collision.layer == LayerMask.NameToLayer("wall"))
-			{
-				// --- walls ---
-				/*// ToDo set platform position
-				PlatformApi.PlatformApi.SetPos();
-				PlatformApi.PlatformApi.SetHome();*/
-				return false;
-			}
-
-			Plugin.logger.LogWarning(collidedObject + ": " + string.Join("\n", collidedObject.GetComponents<object>()));
-
-			if (collision.layer != (int)playerLayerField.GetValue(__instance))
-			{
-				if (collision.colliderPP.monobehaviourCollider.inverseMass <= Fix.Zero) return true;
-
-				// --- everything else (missile, etc) ---
-				BoplBody body = collidedObject.GetComponent<BoplBody>();
-				body.position = holePair.dCircle.position + Vec2.NormalizedSafe(body.velocity) * holeRadius * (__instance.influenceRadiusMultiplier / (Fix)1.5);
-				body.velocity = body.StartVelocity;
-
-				return false;
-			}
-
-			// --- player ---
-
 			PlayerCollision playerCollision = collidedObject.GetComponent<PlayerCollision>();
 
 			if (playerCollision == null)
 			{
 				// player is ball
 				BoplBody body = collidedObject.GetComponent<BoplBody>();
-				body.position = holePair.dCircle.position + Vec2.NormalizedSafe(body.velocity) * holeRadius;
-				return false;
+				body.position = pair.dCircle.position + Vec2.NormalizedSafe(body.velocity) * pair.dCircle.radius;
+				return;
 			}
 
 			PlayerBody playerBody = bodyField.GetValue(playerCollision) as PlayerBody;
 			PlayerPhysics physics = physicsField.GetValue(playerCollision) as PlayerPhysics;
 
 			physics.UnGround(true, false);
-			playerBody.position = holePair.dCircle.position + Vec2.NormalizedSafe(playerBody.Velocity) * holeRadius;
+			playerBody.position = pair.dCircle.position + Vec2.NormalizedSafe(playerBody.Velocity) * pair.dCircle.radius;
+		}
 
+		private static void TeleportObject(GameObject collidedObject, BlackHole pair)
+		{
+			BoplBody body = collidedObject.GetComponent<BoplBody>();
+			body.position = pair.dCircle.position + Vec2.NormalizedSafe(body.velocity) * pair.dCircle.radius * (pair.influenceRadiusMultiplier / (Fix)1.5);
+			body.velocity = body.StartVelocity;
+		}
+
+		private static void EatOtherHole(GameObject collidedObject, BlackHole blackHole, Fix mass)
+		{
+			BlackHole component = collidedObject.GetComponent<BlackHole>();
+			Fix compMass = GetMass(component);
+			if (mass > compMass || (compMass == mass && blackHole.spriteRen.sortingOrder > component.spriteRen.sortingOrder))
+			{
+				// eated another blackhole
+				RemoveConnection(component, false);
+				whiteHoles.Remove(component);
+				holePairs.Remove(component);
+			}
+		}
+
+		private static void TeleportPlatform(GameObject collidedObject, BlackHole blackHole)
+		{
+			// --- walls ---
+			/*// ToDo set platform position
+			PlatformApi.PlatformApi.SetPos();
+			PlatformApi.PlatformApi.SetHome();*/
+		}
+
+		internal static bool BlackHoleCollide_Prefix(BlackHole __instance, CollisionInformation collision)
+		{
+			Fix mass = GetMass(__instance);
+			if (mass < Fix.Zero) return true;
+
+			GameObject collidedObject = collision.colliderPP.fixTrans.gameObject;
+
+			if (collision.layer == (int)affectorLayerField.GetValue(__instance))
+			{
+				EatOtherHole(collidedObject, __instance, mass);
+				return true;
+			}
+
+			// only run original code if this hole isnt paired
+			if (!holePairs.TryGetValue(__instance, out BlackHole holePair) || holePair == null) return true;
+
+			if (collision.layer == LayerMask.NameToLayer("wall"))
+			{
+				TeleportPlatform(collidedObject, __instance);
+				return false;
+			}
+
+			Plugin.logger.LogWarning($"{collidedObject}: {string.Join("\n", collidedObject.GetComponents<object>())}");
+
+			if (collision.layer != (int)playerLayerField.GetValue(__instance))
+			{
+				if (collision.colliderPP.monobehaviourCollider.inverseMass <= Fix.Zero) return true;
+
+				TeleportObject(collidedObject, holePair);
+				return false;
+			}
+
+			TeleportPlayer(collidedObject, holePair);
 			return false;
 		}
 
@@ -146,6 +158,7 @@ namespace Wormhole
 
 			if (!whiteHoles.Add(__instance)) return;
 
+			// ---- debug ----
 			LineRenderer lineRenderer = __instance.gameObject.AddComponent<LineRenderer>();
 			lineRenderer.material = new Material(Shader.Find("Hidden/Internal-Colored"));
 			lineRenderer.endColor = lineRenderer.startColor = Color.red;
